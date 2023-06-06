56639f5b8f9fdd19d22ab27142b90d43aec5158e
Germán Martínez Díaz <121247126+Yermanama@users.noreply.github.com>
2023-06-06 14:28:39 +0000

    Ejercicio de colecciones, desde el ipad.

---
+++ 06Colecciones/rangos.py
@@ -0,0 +1,13 @@
+for numero in range(0,10):
+    if numero % 3 == 0:
+       print(numero)
+
+rango = []
+for numero in range(2, 7):
+    rango.append(numero)
+
+print(rango)
+
+for numero in range(2,10, 3):
+    print(numero)
+
