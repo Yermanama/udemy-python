159d12372bd5b77ba39fe19f0b447b3737b69866
Yermanama <martinezdiazgerman9@gmail.com>
2023-06-07 07:57:50 +0200

    Trabajo en ciclos

---
+++ 06Ciclos/cinco_descendente.py
@@ -0,0 +1,8 @@
+contador = 5
+minimo = 1
+
+while contador >= 1:
+    print(contador)
+    contador -= 1
+else:
+    print('Ciclo while terminado y ejercicio terminado.')
---
+++ 06Ciclos/imprimir_cinco.py
@@ -0,0 +1,7 @@
+contador: int = 1
+
+while contador <= 5:
+    print(contador)
+    contador += 1
+else:
+    print('Ya ha terminado el ciclo. Ejercicio resuelto')
