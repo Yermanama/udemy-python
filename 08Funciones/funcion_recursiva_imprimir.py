f451b702450fb4c82036b9d7d72ac3c15bc713d6
Yermanama <martinezdiazgerman9@gmail.com>
2023-06-09 09:36:16 +0200

    Ejercicio impresion numeros de manera recursiva

---
+++ 08Funciones/funcion_recursiva_imprimir.py
@@ -0,0 +1,26 @@
+"""
+Imprimir numeros de 5 a 1 de manera descendente usando funciones recursivas.
+Puede ser cualquier valor positivo, ejemplo, si pasamos el valor de 5, debe imprimir:
+5
+4
+3
+2
+1
+
+En caso de pasar el valor de 3, debe imprimir:
+3
+2
+1
+
+Si se pasan valores negativos no imprime nada
+"""
+
+def imprimir_numeros_recursivamente(numero : int):
+    while numero > 0:
+        print(numero)
+        numero -= 1
+
+if __name__ == "__main__":
+    imprimir_numeros_recursivamente(5)
+    imprimir_numeros_recursivamente(3)
+    imprimir_numeros_recursivamente(-1)
