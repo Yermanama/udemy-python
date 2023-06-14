66c93e7e2e891965976409485b5c28503eec5909
Yermanama <martinezdiazgerman9@gmail.com>
2023-06-14 10:17:01 +0200

    Constantes en python

---
+++ 10VariablesClase/test_constantes.py
@@ -0,0 +1,11 @@
+from constantes import *
+# Podrías cambiar la manera en la que importamos el módulo
+# from constantes import Matematicas as Mate
+
+print(CONSTANTE)
+print(Matematicas.PI)
+
+# En realidad, en python si que podemos cambiar el valor, porque no existe el concepto de constante
+# NO DEBEMOS DE HACERLO
+"""CONSTANTE = 'Nuevo valor'
+print(CONSTANTE)"""
