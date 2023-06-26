b773de8cedce73bc0560be55989e5f931038dff8
Yermanama <martinezdiazgerman9@gmail.com>
2023-06-26 16:02:03 +0200

    Termino primer archivo de bool

--- ProfundizandoPython/Inicial/tipo_bool.py
+++ ProfundizandoPython/Inicial/tipo_bool.py
@@ -20,3 +20,24 @@ print(f'En el caso de cadena vacía el resultado es -> {bool(cadena)}')
 
 cadena = 'hola'
 print(f'En el caso de cadena llena, el resultado es -> {bool(cadena)}')
+
+
+# Tipo colecciones, false para colecciones vacías, true para el resto
+
+lista = []
+print(f'Para el caso de la lista vacía, el resultado es -> {bool(lista)}')
+
+lista = ['hola']
+print(f'Para el caso de una lista llena, el resultado es {bool(lista)}')
+
+tupla = ()
+print(f'Para el caso de una tupla vacía, el resultado es {bool(tupla)}')
+
+tupla = (1, 2)
+print(f'Para el caso de una tupla llena, el resultado es -> {bool(tupla)}')
+
+diccionario = {}
+print(f'Para el caso de un diccionario vacío, el resultado es {bool(diccionario)}')
+
+diccionario = {'hola': 1}
+print(f'Para el caso de un diccionario lleno, el resultado es -> {bool(diccionario)}')
