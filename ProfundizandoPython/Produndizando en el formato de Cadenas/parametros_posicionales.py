a55ccd36ee8654fab4209be13e871ddd02287daa
Yermanama <martinezdiazgerman9@gmail.com>
2023-06-27 18:52:57 +0200

    Profundizando en el formato de cadenas

---
+++ ProfundizandoPython/Produndizando en el formato de Cadenas/parametros_posicionales.py
@@ -0,0 +1,24 @@
+# Profundizando el tipo str
+
+# Dar formato a un str
+
+nombre = 'Juan'
+edad = 38
+mensaje_con_formato = 'Mi nombre es %s '%nombre
+
+print(mensaje_con_formato)
+
+mensaje_con_formato = 'Mi nombre es %s y tengo %d años'%(nombre, edad)
+print(mensaje_con_formato)
+
+
+persona = ('Karla', 'Gomez', 5000.00)
+mensaje_con_formato = 'Hola %s %s. Tu sueldo es %.2f'%persona
+print(mensaje_con_formato)
+
+mensaje_con_formato = 'Hola %s %s. Tu sueldo es %.2f'
+print(mensaje_con_formato%persona)
+
+# Sustituir strings %s
+# Sustituir ints %d
+# Sustituir float %d
